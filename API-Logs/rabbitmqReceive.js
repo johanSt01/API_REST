ceafef8c5d756bd328bc2445a45743a96c735f56
Johan Stiven <johans.londonoa1@uqvirtual.edu.co>
2024-04-27 15:05:41 -0500

    implementacion de mongoDB y conexión

--- API-Logs/rabbitmqReceive.js
+++ API-Logs/rabbitmqReceive.js
@@ -1,9 +1,18 @@
 import * as amqp from 'amqplib';
-import fs from 'fs';
-import path from 'path';
+import { MongoClient } from 'mongodb';
+import { config } from 'dotenv';
+config();
 
-// Ruta donde se guardarán los logs
-const logsFolder = './Logs';
+// Obtener la dirección IP y el puerto desde las variables de entorno
+const host = process.env.MONGODB_HOST;
+const port = process.env.MONGODB_PORT;
+const username = process.env.MONGODB_USER;
+const password = process.env.MONGODB_PASSWORD;
+// URL de conexión a tu instancia de MongoDB
+const url = `mongodb://${username}:${password}@${host}:${port}/?authSource=admin`;
+// Nombre de la base de datos
+const dbName = process.env.MONGODB_DATABASE
+
 
 async function callback(msg) {
     const message_info = msg.content.toString('utf-8').split('#');
@@ -14,33 +23,56 @@ async function callback(msg) {
     console.log(`FECHA: ${message_info[4]}`);
     console.log(`ACCION: ${message_info[5]}`);
 
-   // Verificar si la carpeta "logs" existe, si no, crearla
-    if (!fs.existsSync(logsFolder)) {
-        fs.mkdirSync(logsFolder, { recursive: true });
-    }
+    const TIPO_DE_LOG = message_info[0];
+    const METODO_HTTP = message_info[1];
+    const APLICACION = message_info[2];
+    const MODULO = message_info[3];
+    const FECHA = message_info[4];
+    const ACCION = message_info[5];
 
-    // Construir el nombre del archivo de log con la ruta de la carpeta
-    const logFilename = path.join(logsFolder, new Date().toISOString().slice(0, 10) + ".log");
+    // Datos a insertar en la colección
+    const data = [
+        { TIPO_DE_LOG: TIPO_DE_LOG, 
+            METODO_HTTP: METODO_HTTP,
+            APLICACION: APLICACION,
+            MODULO: MODULO,
+            FECHA: FECHA,
+            ACCION: ACCION}
+    ];
 
-    console.log('nombre del archivo log', logFilename);
+    // Llamar a la función para insertar datos
+    insertData(data);
 
-    // Escribir el mensaje de log en el archivo
-    fs.appendFileSync(logFilename, `TIPO DE LOG: ${message_info[0]}\n`);
-    fs.appendFileSync(logFilename, `METODO-HTTP: ${message_info[1]}\n`);
-    fs.appendFileSync(logFilename, `APLICACION: ${message_info[2]}\n`);
-    fs.appendFileSync(logFilename, `MODULO: ${message_info[3]}\n`);
-    fs.appendFileSync(logFilename, `FECHA: ${message_info[4]}\n`);
-    fs.appendFileSync(logFilename, `ACCION: ${message_info[5]}\n\n`);
 }
+
+async function insertData(data) {
+    const client = new MongoClient(url);
+  
+    try {
+      await client.connect();
+      console.log('Conectado correctamente al servidor de MongoDB');
+  
+      const db = client.db(dbName);
+      const collection = db.collection('Logs');
+  
+      // Insertar datos en la colección
+      const result = await collection.insertMany(data);
+      console.log(`${result.insertedCount} documentos insertados correctamente`);
+
+    } catch (error) {
+      console.error('Error al conectar o insertar datos:', error);
+    } finally {
+      // Cerrar la conexión al finalizar
+      await client.close();
+      console.log('Conexión cerrada');
+    }
+  }
 
 async function receiveMessage() {
     try {
         console.log("Conectando a RabbitMQ...");
         const connection = await amqp.connect('amqp://rabbitmq');
         const channel = await connection.createChannel();
-
-        // Eliminar la cola existente
-        await channel.deleteQueue('logs');
 
         // Declarar la cola
         const queue = 'logs';
