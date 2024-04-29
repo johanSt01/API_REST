20ba7453c1124a548224687ab1b9f013f3ad2480
Johan Stiven <johans.londonoa1@uqvirtual.edu.co>
2024-04-29 16:21:35 -0500

    api logs completa

--- API-Logs/routes.js
+++ API-Logs/routes.js
@@ -1,8 +1,7 @@
-import express from 'express';
+import express, { application } from 'express';
 import mongoose from 'mongoose';
 import Log from './util.js';
-//import { enviarMensaje } from './rabbitmqReceive.js';
-//import { getDataFilteredByDate, getLogsFilteredByDate, getLogsFilteredByType } from './util.js';
+import { sendMessage } from '../src/rabbitmqService.js';
 import { config } from 'dotenv';
 config();
 
@@ -74,5 +73,79 @@ app.get('/logs', async (req, res) => {
     } catch (error) {
         console.error('Error al recuperar los logs:', error);
         res.status(500).json({ error: 'Error al recuperar los logs' });
+    }
+});
+
+
+//Metodo rest para obtener los logs por aplicación
+app.get('/logs/:application', async (req, res) =>{
+    try{
+        //tipo de aplicación a filtrar
+        const application = req.params.application;
+
+        // Parámetros de paginación
+        const page = parseInt(req.query.page) || 1;
+        const perPage = parseInt(req.query.perPage) || 10;
+        
+        // Parámetros de ordenamiento
+        const sortField = req.query.sortBy || 'FECHA';
+        const sortOrder = req.query.order || 'asc';
+        const sortOptions = { [sortField]: sortOrder };
+
+        const query = { APLICACION: application };
+
+        // Filtrar por fechas
+        if (req.query.startDate) {
+            query.FECHA = { ...query.FECHA, $gte: new Date(req.query.startDate) };
+        }
+        if (req.query.endDate) {
+            query.FECHA = { ...query.FECHA, $lte: new Date(req.query.endDate) };
+        }
+
+        // Filtrar por tipo de log
+        if (req.query.logType) {
+            query.TIPO_DE_LOG = req.query.logType;
+        }
+
+        // Contar total de logs
+        const totalLogs = await Log.countDocuments(query);
+        
+        const logs = await Log.find(query)
+            .sort(sortOptions)
+            .limit(perPage)
+            .skip((page - 1) * perPage);
+        
+        res.json({
+            page,
+            perPage,
+            totalLogs,
+            logs
+        });
+
+    }catch(error) {
+        console.error('Error al recuperar los logs por aplicacion: ', error);
+        res.status(500).json({ error: 'Error al recuperar los logs por aplicacion' });
+    }
+});
+
+
+// Ruta POST para crear un nuevo log
+app.post('/logs', async (req, res) => {
+    try {
+        //Mensaje de los logs
+        const tipo_log = "INFO";
+        const metodo = "POST";
+        const application = "LOGS_API_REST";
+        const modulo = "routes.js"
+        const fecha = new Date().toISOString();
+        const mensaje = "LOG CREADO CORRECTAMENTE DESDE LA API DE LOGS";
+        //Enviar mensaje
+        await sendMessage(tipo_log, metodo,application, modulo, fecha, mensaje);
+
+        // Devolver la respuesta con el log creado
+        res.status(201).json('Log creado correctamente');
+    }catch(error){
+        console.error('Error al crear el log:', error);
+        res.status(500).json({ error: 'Error al crear el log' });
     }
 });
